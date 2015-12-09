473fdad0e1754d4ddca2bc560fece36fdfd916fa
rafaelbelarmino <rafaelon@live.com>
2015-12-09 17:41:03 -0300

    niovo comit

---
+++ SampleSplashScreen/App_n/app/src/main/java/br/edu/ifpb/app_n/Splash.java
@@ -0,0 +1,15 @@
+package br.edu.ifpb.app_n;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Rafael on 25/11/2015.
+ */
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ SampleSplashScreen/App_n/app/src/main/java/br/edu/ifpb/app_n/SplashActivityFragment.java
@@ -0,0 +1,22 @@
+package br.edu.ifpb.app_n;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SplashActivityFragment extends Fragment {
+
+    public SplashActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+}
